68c6f809c08058e0619a9ed5d2adb0c6fb42684d
sakichang0226 <saki.develop0226@gmail.com>
2026-05-11 00:07:16 +0900

    refactor: JwtToPrincipalConverterをstaticメソッド化、JwtDecoderにJavadoc追加

--- common/src/main/java/com/project/abydos/saki/common/util/JwtDecoder.java
+++ common/src/main/java/com/project/abydos/saki/common/util/JwtDecoder.java
@@ -1,4 +1,25 @@
 package com.project.abydos.saki.common.util;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.project.abydos.saki.common.config.JwtProperties;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * JWTデコーダー.
+ * HMAC256アルゴリズムでトークンの署名検証と有効期限チェックを行う。
+ */
+@Component
+@RequiredArgsConstructor
 public class JwtDecoder {
+
+    private final JwtProperties properties;
+
+    public DecodedJWT decode(@NonNull String token) {
+        return JWT.require(Algorithm.HMAC256(properties.getSecret()))
+                .build().verify(token);
+    }
 }
---
+++ common/src/main/java/com/project/abydos/saki/common/util/JwtToPrincipalConverter.java
@@ -0,0 +1,47 @@
+package com.project.abydos.saki.common.util;
+
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.project.abydos.saki.common.constant.SecurityConstant;
+import com.project.abydos.saki.common.entity.UserPrincipal;
+import lombok.NonNull;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+/**
+ * JWTからUserPrincipalへの変換クラス.
+ * デコード済みJWTのclaimsからユーザーID、メールアドレス、権限情報を抽出し、
+ * Spring Securityの認証プリンシパルを生成する。
+ */
+public class JwtToPrincipalConverter {
+
+    private JwtToPrincipalConverter() {}
+
+    /**
+     * デコード済みJWTからUserPrincipalを生成する.
+     *
+     * @param jwt デコード・検証済みのJWTトークン
+     * @return 認証プリンシパル（userId, email, authorities を保持）
+     */
+    public static UserPrincipal convert(@NonNull DecodedJWT jwt) {
+        return UserPrincipal.builder()
+                .userId(Long.valueOf(jwt.getSubject()))
+                .email(jwt.getClaim(SecurityConstant.CLAIM_EMAIL).asString())
+                .authorities(extractAuthorities(jwt))
+                .build();
+    }
+
+    /**
+     * JWTのclaimから権限リストを抽出する.
+     * claim "a" が存在しない場合は空リストを返す。
+     *
+     * @param jwt デコード済みのJWTトークン
+     * @return 権限リスト
+     */
+    private static List<SimpleGrantedAuthority> extractAuthorities(@NonNull DecodedJWT jwt) {
+        Claim claim = jwt.getClaim(SecurityConstant.CLAIM_AUTHORITIES);
+        if (claim.isNull() || claim.isMissing()) return List.of();
+        return claim.asList(SimpleGrantedAuthority.class);
+    }
+}
