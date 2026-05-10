5744a2144df04544718081d47f9fc68a8dbc389b
sakichang0226 <saki.develop0226@gmail.com>
2026-05-11 00:07:26 +0900

    docs: UserPrincipal/UserPrincipalAuthenticationTokenにJavadoc追加

--- common/src/main/java/com/project/abydos/saki/common/entity/UserPrincipalAuthenticationToken.java
+++ common/src/main/java/com/project/abydos/saki/common/entity/UserPrincipalAuthenticationToken.java
@@ -1,4 +1,27 @@
 package com.project.abydos.saki.common.entity;
 
-public class UserPrincipalAuthenticationToken {
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+
+/**
+ * JWT認証済みユーザーの認証トークン.
+ * JwtAuthenticationFilterで検証成功後にSecurityContextへ設定される。
+ */
+public class UserPrincipalAuthenticationToken extends AbstractAuthenticationToken {
+    private final UserPrincipal userPrincipal;
+
+    public UserPrincipalAuthenticationToken(UserPrincipal userPrincipal) {
+        super(userPrincipal.getAuthorities());
+        this.userPrincipal = userPrincipal;
+        setAuthenticated(true);
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public UserPrincipal getPrincipal() {
+        return userPrincipal;
+    }
 }
